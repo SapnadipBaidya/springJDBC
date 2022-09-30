fec91e76c159ca4a1e31196c0ccdddf7d846fed7
Sapnadip Baidya <65809505+SapnadipBaidya@users.noreply.github.com>
2022-09-30 14:15:42 +0530

    Add files via upload

---
+++ src/test/java/org/jdbc/spring/SpirngJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jdbc.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
